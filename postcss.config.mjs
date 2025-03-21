8511cb3d1b398034bf740f8cb99c2ffc4a3f9f7b
Zack Agba <zackagba7@gmail.com>
2025-03-21 18:33:20 +0100

    fix(profile page): add new components & bug fixes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {}, // Use the new package
+    autoprefixer: {}, // Optional: Add autoprefixer if needed
   },
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss"
 
 export default {
-  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
